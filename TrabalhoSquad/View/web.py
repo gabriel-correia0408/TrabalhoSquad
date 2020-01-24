c710c17a1c248329522be71f43a0a932fda6f0f7
GABRIEL <gabrielmal38@gmail.com>
2020-01-24 11:03:10 -0300

    resolvendo trabalho

--- TrabalhoSquad/View/web.py
+++ TrabalhoSquad/View/web.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect
 import sys
-sys.path.append(r'C:\Users\900135\Desktop\TrabalhoSquad')
+sys.path.append(r'C:\Users\900137\Desktop\TrabalhoSquad')
 from Controller.squad_controller import SquadController
 from Model.squad import Squad
 
@@ -20,41 +20,41 @@ def ler():
 
 @app.route('/cadastrar')
 def inserir():
-    squad = Squad()
-    squad_control = SquadController()
-    squad.Nome = 'TESTE'
-    squad.descricao = 'testando'
-    squad.NumeroPessoas = 20
-    squad.LinguagemBackEnd = 'testando'
-    squad.FrameworkFrontEnd= 'angular'
-    squad_control.inserir(squad)
-    if 'id' in request.args:
-        id = request.args['id']
-        squad = squad_controller.inserir(id)
+    # squad = Squad()
+    # squad_control = SquadController()
+    # squad.Nome = ''
+    # squad.descricao = 'testando'
+    # squad.NumeroPessoas = 20
+    # squad.LinguagemBackEnd = 'testando'
+    # squad.FrameworkFrontEnd= 'angular'
+    # squad_control.inserir(squad)
+    # if 'id' in request.args:
+    #     id = request.args['id']
+    #     squad = squad_controller.inserir(id)
     return render_template('cadastrar.html', titulo_app = nome, squad = squad )
 
 
 @app.route('/deletar')
 def deletar():
     id = int(request.args['id'])
-    squad_controller.deletar(id)
-    if id_end != 'None':
-        end_controller.deletar(id_end)
+    squad_controller.deletar(id)    
     return redirect('/listar')
 
 @app.route('/salvar')
 def salvar():
     squad = Squad()
-    squad.id = request.args['id']
+    # squad.id = request.args['id']
     squad.Nome = request.args['Nome']
     squad.Descricao = request.args['Descricao']
     squad.NumeroPessoas = request.args['NumeroPessoas']
     squad.LinguagemBackEnd = request.args['LinguagemBackEnd']
     squad.FrameworkFrontEnd = request.args['FrameworkFrontEnd']
     
+    squad_controller.inserir(squad)
     
-    
-    return squad_controller.inserir(squad)
+    return redirect ('/listar')
+
+# def editar():
 
 
 app.run(debug=True)
